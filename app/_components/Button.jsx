3cd4680ed2e4925b492a0949aed6f8b9af66bdce
Ryan <ryan@ryansolomon.io>
2024-07-16 07:02:58 -0600

    components

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -7,13 +7,17 @@ const Button = () => {
   };
 
   return (
-    <button
+    <>
+          <button
       onClick={handleClick}
       className="px-4 py-2 rounded bg-blue-600 text-white hover:bg-blue-700"
     >
       Click Me
     </button>
+    </>
   );
 };
 
 export default Button;
+
+
